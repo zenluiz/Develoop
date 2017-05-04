504f04d663131dc6353eb7cdd0f35153fa858e90
Irwing Naranjo <khalisser@gmail.com>
2017-05-04 09:49:32 -0400

    Estructura inicial

--- index.php
+++ index.php
@@ -3,13 +3,116 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Develoop | Ciudad de vacaciones</title>
-		<link rel="stylesheet" href="/assets/css/animate.css">
-		<link rel="stylesheet" href="/assets/css/bootstrap.min.css">
-		<link rel="stylesheet" href="/assets/css/bootstrap-theme.min.css">
-		<link rel="stylesheet" href="/assets/css/style.css">
+		<link rel="stylesheet" href="assets/css/animate.css">
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+		<link rel="stylesheet" href="assets/css/bootstrap-theme.min.css">
+		<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+		<link rel="stylesheet" href="assets/css/style.css">
 	</head>
 	<body>
-		<script src="/assets/js/jquery-1.11.2.min.js"></script>
-		<script src="/assets/js/bootstrap.min.js"></script>
+		<!-- #1 header-top -->
+		<section class="header-top">
+			<div class="container">
+				<div class="row">
+					<!-- header-top-right -->
+					<div class="col-sm-6">
+						<select class="select-language" name="language">
+							<option value="0">Idioma</option>
+							<option value="1">Español</option>
+							<option value="2">Inglés</option>
+						</select>
+						<span class="text-call">Llámanos <a href="tel:+34972256212">+34 972 25 62 12</a></span>
+					</div>
+
+					<!-- header-top-left -->
+					<div class="col-sm-6">
+						<ul class="links-socials">
+							<li><a target="__blank" href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a target="__blank" href="#"><i class="fa fa-twitter"></i></a></li>
+							<li><a target="__blank" href="#"><i class="fa fa-youtube"></i></a></li>
+							<li><a target="__blank" href="#"><i class="fa fa-instagram"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- #2 nav -->
+		<section class="nav"></section>
+
+		<!-- #3 banner -->
+		<section class="banner">
+			<div class="shadow animated fadeInLeft"></div>
+			<div class="container">
+				<div class="action">
+					<h2 class="animated fadeInLeft">Develoop</h2>
+					<h4 class="animated fadeInLeft">¡ciudad de vanaciones, disfrute y bendiciones!</h4>
+					<button class="btn btn-theme-action animated fadeInLeft">Contactanos</button>
+				</div>
+			</div>
+		</section>
+
+		<!-- #4 content -->
+		<section class="content">
+			<div class="container">
+				<div class="row">
+					<section class="description">
+						<p class="text-center">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi <strong>ut aliquip ex ea commodo</strong> consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+						</p>
+					</section>
+
+					<div class="img-circle">
+						<div class="circle"></div>
+					</div>
+
+					<div class="details">
+						<div class="col-sm-6">
+							<div class="row">
+								<div class="col-sm-12"><img src="assets/img/img2.jpg" class="img-responsive" alt="img4"></div>
+								<div class="col-sm-12"><img src="assets/img/img1.jpg" class="img-responsive" alt="img3"></div>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="row">
+								<div class="col-sm-6"><img src="assets/img/img3.jpg" class="img-responsive" alt="img3"></div>
+								<div class="col-sm-6"><img src="assets/img/img4.jpg" class="img-responsive" alt="img4"></div>
+								<div class="col-sm-12">
+									<div class="details-text">
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+										<h3>SU FELICIDAD</h3>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="img-larga">
+				<div class="img-larga" style="background:#f1f1f1;height:120px;width:100%;"></div>
+			</div>
+		</section>
+
+		<!-- #4 content -->
+		<section class="sponsors">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-3"><div class="img-larga" style="background:#f1f1f1;height:180px;border:1px solid #222;width:100%;"></div></div>
+					<div class="col-sm-3"><div class="img-larga" style="background:#f1f1f1;height:180px;border:1px solid #222;width:100%;"></div></div>
+					<div class="col-sm-3"><div class="img-larga" style="background:#f1f1f1;height:180px;border:1px solid #222;width:100%;"></div></div>
+					<div class="col-sm-3"><div class="img-larga" style="background:#f1f1f1;height:180px;border:1px solid #222;width:100%;"></div></div>
+				</div>
+			</div>
+		</section>
+
+		<!-- #5 footer -->
+		<section class="footer"></section>
+
+		<script src="assets/js/jquery-1.11.2.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
 	</body>
 </html>
