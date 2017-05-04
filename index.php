c4960bf080be89adcbb9a02cfb370c14f3f0b63a
Irwing Naranjo <khalisser@gmail.com>
2017-05-03 22:08:41 -0400

    Index y style.css

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Develoop | Ciudad de vacaciones</title>
+		<link rel="stylesheet" href="/assets/css/animate.css">
+		<link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/assets/css/bootstrap-theme.min.css">
+		<link rel="stylesheet" href="/assets/css/style.css">
+	</head>
+	<body>
+		<script src="/assets/js/jquery-1.11.2.min.js"></script>
+		<script src="/assets/js/bootstrap.min.js"></script>
+	</body>
+</html>
