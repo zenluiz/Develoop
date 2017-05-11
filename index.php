adee331ffbb67f68e2d5759d6162726a7040f6c4
Irwing Naranjo <khalisser@gmail.com>
2017-05-10 21:05:33 -0400

    Últimos avances

--- index.php
+++ index.php
@@ -3,11 +3,13 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Develoop | Ciudad de vacaciones</title>
-		<link rel="stylesheet" href="assets/css/animate.css">
 		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
 		<link rel="stylesheet" href="assets/css/bootstrap-theme.min.css">
 		<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+		<link rel="stylesheet" href="assets/css/animate.css">
 		<link rel="stylesheet" href="assets/css/style.css">
+		<link href="https://fonts.googleapis.com/css?family=Quicksand:400,700" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Dancing+Script:400,700" rel="stylesheet">
 	</head>
 	<body>
 		<!-- #1 header-top -->
@@ -25,7 +27,7 @@
 					</div>
 
 					<!-- header-top-left -->
-					<div class="col-sm-6">
+					<div class="col-sm-6 hidden-xs">
 						<ul class="links-socials">
 							<li><a target="__blank" href="#"><i class="fa fa-facebook"></i></a></li>
 							<li><a target="__blank" href="#"><i class="fa fa-twitter"></i></a></li>
@@ -33,12 +35,105 @@
 							<li><a target="__blank" href="#"><i class="fa fa-instagram"></i></a></li>
 						</ul>
 					</div>
+
+					<!-- header-top-nav-roles -->
+					<div class="col-sm-12">
+						<div class="text-right nav-roles">
+							<select class="btn btn-primary select-roles" name="roles">
+								<option><a href="#">Área cliente</a></option>
+								<option><a href="#">Área administrador</a></option>
+							</select>
+						</div>
+					</div>
 				</div>
 			</div>
 		</section>
+		<div class="clearfix"></div>
 
 		<!-- #2 nav -->
-		<section class="nav"></section>
+		<section class="nav">
+			<nav class="navbar navbar-default nav-responsive hidden-lg" role="navigation">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive">
+						    <span class="sr-only">Toggle navigation</span>
+						    <span class="icon-bar"></span>
+						    <span class="icon-bar"></span>
+						    <span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">Develoop</a>
+					</div>
+
+					<div class="collapse navbar-collapse navbar-responsive">
+						<ul class="nav navbar-nav">
+							<li><a href="#">Develoop</a></li>
+							<li><a href="#">Alojamiento</a></li>
+							<li><a href="#">Ofertas</a></li>
+							<li><a href="#">Instalaciones y servicios</a></li>
+							<li><a href="#">Actividades</a></li>
+							<li><a href="#">Restauración</a></li>
+							<li><a href="#">Entorno</a></li>
+							<li><a href="#">Galería</a></li>
+							<li><a href="#">Noticias</a></li>
+						</ul>
+					</div>
+			</nav>
+			<div class="clearfix"></div>
+
+			<nav class="navbar nav-primary visible-lg">
+				<div class="container">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand visible-xs" href="#">Develoop</a>
+					</div>
+
+					<div class="collapse navbar-collapse">
+						<ul class="nav navbar-nav">
+							<li><a href="#">Develoop</a></li>
+							<li><a href="#">Alojamiento</a></li>
+							<li><a href="#">Ofertas</a></li>
+							<li><a href="#">Instalaciones y servicios</a></li>
+							<li><a href="#">Actividades</a></li>
+							<li><a href="#">Restauración</a></li>
+							<li><a href="#">Entorno</a></li>
+							<li><a href="#">Galería</a></li>
+							<li><a href="#">Noticias</a></li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+			<div class="clearfix"></div>
+
+			<nav class="navbar nav-secondary visible-lg">
+				<div class="container">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand visible-xs" href="#">Develoop</a>
+					</div>
+
+					<div class="collapse navbar-collapse">
+						<ul class="nav navbar-nav">
+							<li><a href="#">Actualidad</a></li>
+							<li><a href="#">Medio ambiente</a></li>
+							<li><a href="#">Localización</a></li>
+							<li><a href="#">Visita virtual</a></li>
+							<li><a href="#">Contacto</a></li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+			<div class="clearfix"></div>
+		</section>
+		<div class="clearfix"></div>
 
 		<!-- #3 banner -->
 		<section class="banner">
@@ -51,6 +146,7 @@
 				</div>
 			</div>
 		</section>
+		<div class="clearfix"></div>
 
 		<!-- #4 content -->
 		<section class="content">
@@ -63,7 +159,9 @@
 					</section>
 
 					<div class="img-circle">
-						<div class="circle"></div>
+						<div class="content-circle">
+							<div class="circle font-dancing-700">Vive sensaciones</div>
+						</div>
 					</div>
 
 					<div class="details">
@@ -93,24 +191,157 @@
 			</div>
 
 			<div class="img-larga">
-				<div class="img-larga" style="background:#f1f1f1;height:120px;width:100%;"></div>
+				<img src="assets/img/bg-montain.jpg" alt="Mountain">
 			</div>
+
 		</section>
 
 		<!-- #4 content -->
 		<section class="sponsors">
 			<div class="container">
 				<div class="row">
-					<div class="col-sm-3"><div class="img-larga" style="background:#f1f1f1;height:180px;border:1px solid #222;width:100%;"></div></div>
-					<div class="col-sm-3"><div class="img-larga" style="background:#f1f1f1;height:180px;border:1px solid #222;width:100%;"></div></div>
-					<div class="col-sm-3"><div class="img-larga" style="background:#f1f1f1;height:180px;border:1px solid #222;width:100%;"></div></div>
-					<div class="col-sm-3"><div class="img-larga" style="background:#f1f1f1;height:180px;border:1px solid #222;width:100%;"></div></div>
+					<div class="col-sm-2">
+						<div class="img-content text-center">
+							<img src="assets/img/sponsor1.png" alt="">
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="img-content text-center">
+							<img src="assets/img/sponsor2.png" alt="">
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="img-content text-center">
+							<img src="assets/img/sponsor3.png" alt="">
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="img-content text-center">
+							<img src="assets/img/sponsor4.png" alt="">
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="img-content text-center">
+							<img src="assets/img/sponsor5.png" alt="">
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="img-content text-center">
+							<img src="assets/img/sponsor6.png" alt="">
+						</div>
+					</div>
 				</div>
 			</div>
 		</section>
 
 		<!-- #5 footer -->
-		<section class="footer"></section>
+		<section class="footer" style="background:#002C5B;">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-3">
+						<h3>DEVELOOP SOFTWARE</h3>
+						<ul>
+							<li>Email: info@develoop.net</li>
+							<li>Llámanos: <a href="tel:340000000000">+34 000 0000 000</a></li>
+							<li>Apt. 6 XXXX Roses (Girona, España)</li>
+						</ul>
+					</div>
+
+					<div class="col-sm-4">
+						<div class="row">
+							<h3>Vinculos de interes</h3>
+							<div class="col-sm-6">
+								<ul>
+									<li><a href="#">Reglamento interno</a></li>
+									<li><a href="#">Trabaja con nosotros</a></li>
+									<li><a href="#">Mapa web</a></li>
+								</ul>
+							</div>
+							<div class="col-sm-6">
+								<ul>
+									<li><a href="#">Aviso legal</a></li>
+									<li><a href="#">Política de privacidad</a></li>
+									<li><a href="#">Política de cookies</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-5">
+						<h3>Recibe nuestro Newsletter</h3>
+						<form class="form-inline">
+							<div class="form-group">
+								<input type="email" class="form-control" name="email" placeholder="Email">
+							</div>
+							<button class="btn btn-default">Enviar</button>
+
+
+							<ul class="links-socials">
+								<li><a target="__blank" href="#"><i class="fa fa-facebook"></i></a></li>
+								<li><a target="__blank" href="#"><i class="fa fa-twitter"></i></a></li>
+								<li><a target="__blank" href="#"><i class="fa fa-youtube"></i></a></li>
+								<li><a target="__blank" href="#"><i class="fa fa-instagram"></i></a></li>
+							</ul>
+						</form>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- #6 reservations -->
+		<section class="reservations" style="background:#019BD9;">
+			<div class="container">
+				<div class="row">
+					<h3>RESERVA CON NOSOTROS</h3>
+					<form class="form-inline">
+						<div class="form-group col-sm-2">
+							<div class="row">
+								<div class="input-group">
+									<input type="text" class="form-control" placeholder="Llegada">
+									<div class="input-group-addon"><i class="fa fa-calendar"></i></div>
+								</div>
+							</div>
+						</div>
+						<div class="form-group col-sm-2">
+							<div class="row">
+								<div class="input-group">
+									<input type="text" class="form-control" placeholder="Salida">
+									<div class="input-group-addon"><i class="fa fa-calendar"></i></div>
+								</div>
+								</div>
+						</div>
+						<div class="form-group col-sm-2">
+							<div class="row">
+								<select class="form-control input-small">
+									<option>Adultos</option>
+									<option>1</option>
+									<option>2</option>
+									<option>3</option>
+									<option>4</option>
+								</select>
+							</div>
+						</div>
+						<div class="form-group col-sm-2">
+							<div class="row">
+								<select class="form-control">
+									<option>Niños</option>
+									<option>1</option>
+									<option>2</option>
+									<option>3</option>
+									<option>4</option>
+								</select>
+							</div>
+						</div>
+						<div class="form-group col-sm-4">
+							<div class="row">
+								<button class="btn btn-primary">Comprobar disponibilidad</button>
+							</div>
+						</div>
+					</form>
+					</div>
+				</div>
+			</div>
+		</section>
 
 		<script src="assets/js/jquery-1.11.2.min.js"></script>
 		<script src="assets/js/bootstrap.min.js"></script>
